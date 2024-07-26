b28396076f196193b1bb981c4cb417d2b880cc0d
Zavents <80129064+Zavents@users.noreply.github.com>
2024-07-26 15:00:45 +0700

    Add files via upload

---
+++ assets/js/scripts.js
@@ -0,0 +1,60 @@
+document.addEventListener('DOMContentLoaded', function () {
+  // Initialize popovers
+  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+  var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl);
+  });
+
+  // Initialize tooltips
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+  });
+
+  // Alert functionality
+  const alertPlaceholder = document.getElementById('liveAlertPlaceholder');
+  const appendAlert = (message, type) => {
+    const wrapper = document.createElement('div');
+    wrapper.innerHTML = [
+      `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+      `   <div>${message}</div>`,
+      '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+      '</div>'
+    ].join('');
+
+    alertPlaceholder.append(wrapper);
+  };
+
+  const alertTrigger = document.getElementById('liveAlertBtn');
+  if (alertTrigger) {
+    alertTrigger.addEventListener('click', () => {
+      appendAlert('Nice, you triggered this alert message!', 'success');
+    });
+  }
+  const toastTrigger = document.getElementById('liveToastBtn')
+  const toastLiveExample = document.getElementById('liveToast')
+  
+  if (toastTrigger) {
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+    toastTrigger.addEventListener('click', () => {
+      toastBootstrap.show()
+    })
+  }
+  const scrollSpy = new bootstrap.ScrollSpy(document.body, {
+    target: '#navbar-example'
+  })
+  document.querySelectorAll('#nav-tab>[data-bs-toggle="tab"]').forEach(el => {
+    el.addEventListener('shown.bs.tab', () => {
+      const target = el.getAttribute('data-bs-target')
+      const scrollElem = document.querySelector(`${target} [data-bs-spy="scroll"]`)
+      bootstrap.ScrollSpy.getOrCreateInstance(scrollElem).refresh()
+    })
+  })
+
+  // Preloader functionality
+  $(document).ready(function() {
+    setTimeout(function() {
+        $(".preloader").fadeOut(500);
+    }, 500);
+  });
+});
